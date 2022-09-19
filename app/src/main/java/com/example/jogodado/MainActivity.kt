6fd86864cf5ae751df25d785ac816c3713b67872
giovana.andre <giovana.andre@alunoescolassociais.org.br>
2022-09-19 17:39:39 -0300

    Jogo dado finalizado

--- app/src/main/java/com/example/jogodado/MainActivity.kt
+++ app/src/main/java/com/example/jogodado/MainActivity.kt
@@ -4,6 +4,7 @@ package com.example.jogardado
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import com.example.jogodado.R
@@ -14,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val botaoJogar: Button = findViewById(R.id.button)
-        botaoJogar.setOnClickListener{
+        botaoJogar.setOnClickListener {
             jogarDado()
         }
     }
@@ -22,15 +23,26 @@ class MainActivity : AppCompatActivity() {
     private fun jogarDado() {
         val dado = Dado(6)
         val jogarDado = dado.jogar()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = jogarDado.toString()
+        val imagemDado: ImageView = findViewById(R.id.imageView)
+
+        when (jogarDado) {
+        1-> imagemDado.setImageResource(R.drawable.dado_1)
+        2-> imagemDado.setImageResource(R.drawable.dado_2)
+        3-> imagemDado.setImageResource(R.drawable.dado_3)
+        4-> imagemDado.setImageResource(R.drawable.dado_4)
+        5-> imagemDado.setImageResource(R.drawable.dado_5)
+        6-> imagemDado.setImageResource(R.drawable.dado_6)
+        }
     }
+
 }
 
-class  Dado(val numeroLados: Int) {
 
-    fun  jogar(): Int {
-        return (1..numeroLados).random()
+    class Dado(val numeroLados: Int) {
 
+        fun jogar(): Int {
+            return (1..numeroLados).random()
+
+        }
     }
-}
+
